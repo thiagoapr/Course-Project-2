5d47cac487ecf80f00ce2bc68f56c8f2f38651e0
Thiago Pastorelli Rodrigues <rodrigues.t@gmail.com>
2016-11-10 22:43:57 -0200

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,11 @@
+# Ready data
+NEI <- readRDS("summarySCC_PM25.rds") 
+SCC <- readRDS("Source_Classification_Code.rds") 
+
+# Sum PM25 by year
+totalPM25 <- with(NEI, tapply(Emissions/10^3,year, sum))
+
+# Bar Plot
+png("plot1.png", width=480, height=480)
+barplot(totalPM25, main="Total PM2.5", ylab="thousand tons")
+dev.off()
